6e97119703b2f8c3ac094397fdab042d3ed36fb7
Utkarsh Khatri <utkarshkc7@gmail.com>
2024-06-09 23:38:48 +0545

    LOGIN PAGE and logout page along with improvement in sign in

--- myapp/newapp/urls.py
+++ myapp/newapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('homea/',views.home,name="home"),
     path('signup/',views.SaveEnquiry,name="signup"),
     path('home/',views.homea,name="homea"),
+    path('login/',views.LoginPage,name='login'),
+    path('logout/',views.LogoutPage,name='logout')
     
 ]
 if settings.DEBUG:
--- myapp/newapp/views.py
+++ myapp/newapp/views.py
@@ -1,31 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 from .models import hall
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     return render(request,"home.html")
 def SaveEnquiry(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         uname=request.POST.get('cust_name')
         email=request.POST.get('cust_email')
-        password=request.POST.get('password')
+        password1=request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            return HttpResponse('The password donot match with each other try again')
+        
         phone=request.POST.get('phn_no')
         if User.objects.filter(username=uname).exists():
             return HttpResponse("Username already taken. Please choose a different username.", status=400)
-        my_user=User.objects.create_user(uname,email,password)
-        my_user.save()
-        return HttpResponse("User has been registered successfully")
-        print(uname,email,password,phone)
-    return render(request, "SignUp.html")
+        else:
+            my_user=User.objects.create_user(uname,email,password1)
+            my_user.save()
+            return redirect('login')
+    return render(request,"SignUp.html")
+    
+def LoginPage(request):
+    if request.method == "POST":
+        email = request.POST.get('cust_email')
+        password = request.POST.get('password')
+        print(email,password)
+        user = authenticate(request, username=email, password=password)
+
+        print(user)
+        if user is not None:
+            login(request,user)
+            return redirect ('home')
+        else:
+            return HttpResponse('Username or Password is incorrect')
+    return render(request, 'login.html',)
 def homea(request):
     products = hall.objects.all()
     context = {'products':products}
 
     return render(request, 'home.html',context)
     
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
+
+
 
 # for blank url
 def index(request):
