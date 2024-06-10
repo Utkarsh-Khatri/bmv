816c1da969c55ac04e3023b2e140964bd5f51290
Utkarsh Khatri <utkarshkc7@gmail.com>
2024-06-10 21:29:37 +0545

    booking

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "bmv",
         "USER": "root",
-        "PASSWORD": "98510MySQL",
+        "PASSWORD": "Rosh@n09",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
